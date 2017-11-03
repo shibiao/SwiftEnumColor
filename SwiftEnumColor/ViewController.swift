6374b93947ed996c1de40894092eb68976a218fb
WalkWithWind <956035125@qq.com>
2017-11-03 09:39:04 +0800

    4

--- SwiftEnumColor/ViewController.swift
+++ SwiftEnumColor/ViewController.swift
@@ -13,7 +13,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        view.backgroundColor = self.changeColor()
+        view.backgroundColor = changeColor()
     }
 
     override func didReceiveMemoryWarning() {
